834fbf712b48e8094865edbd32b765eaeb0b414b
Pranto Mollick <prantocse01@gmail.com>
2022-04-28 01:47:34 +0600

    adding a post grid and post items

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -4,6 +4,7 @@ import classes from './featured-posts.module.css';
 function FeaturedPosts() {
     return <section className={classes.latest}>
         <h2>Featured posts</h2>
+        
     </section>
 }
 
--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -1,5 +1,21 @@
-function PostItem() {
+import Link from 'next/link';
 
+
+function PostItem() {
+    return <li>
+        <Link>
+            <a>
+                <div>
+                    <Image />
+                </div>
+                <div>
+                    <h3>TITLE</h3>
+                    <time>July 13th 2022</time>
+                    <p>The excert</p>
+                </div>
+            </a>
+        </Link>
+    </li>
 }
 
 export default PostItem;
