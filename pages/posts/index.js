856dc3f9cf4c602c67e6aa94a369cf4c2616dc8e
Pranto Mollick <prantocse01@gmail.com>
2022-04-28 23:39:02 +0600

    all posts showing...

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,33 @@
+import AllPosts from "../../components/posts/all-posts";
+
+
+const DUMMY_POSTS = [
+  {
+    slug: 'getting-started-with-nextjs-1',
+    title: 'Getting started with nextjs',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with build-in SSR.',
+    date: '2022-02-10'
+  },
+  {
+    slug: 'getting-started-with-nextjs-2',
+    title: 'Getting started with nextjs',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with build-in SSR.',
+    date: '2022-02-10'
+  },
+  {
+    slug: 'getting-started-with-nextjs-3',
+    title: 'Getting started with nextjs',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with build-in SSR.',
+    date: '2022-02-10'
+  }
+];
+
+
 function AllPostsPage() {
-    return;
+    return <AllPosts posts={DUMMY_POSTS} />;
 }
   
   export default AllPostsPage;
